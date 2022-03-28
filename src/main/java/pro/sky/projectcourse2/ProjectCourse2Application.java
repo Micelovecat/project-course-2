895097aa20f17e7eceb0af1d22d9cb9ccb48f473
Micelovecat <flora_bux@mail.ru>
2022-03-28 23:07:43 +0300

    First commit

---
+++ src/main/java/pro/sky/projectcourse2/ProjectCourse2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.projectcourse2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCourse2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCourse2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/projectcourse2/ProjectCourse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.projectcourse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCourse2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
