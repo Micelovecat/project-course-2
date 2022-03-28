e2480e47302087a02b326bfadf67e4bccaed897e
Micelovecat <flora_bux@mail.ru>
2022-03-28 23:09:16 +0300

    Second commit

--- src/main/java/pro/sky/projectcourse2/ProjectCourse2Application.java
+++ src/main/java/pro/sky/projectcourse2/ProjectCourse2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjectCourse2Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProjectCourse2Application.class, args);
 	}
 
